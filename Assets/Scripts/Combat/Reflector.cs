a6073a509bc7edb19da2cc2c11566a409e9b3a1f
Red <redstonegameraa@gmail.com>
2022-10-12 19:23:56 -0400

    Fixed small bugs in reflector

--- Assets/Scripts/Combat/Reflector.cs
+++ Assets/Scripts/Combat/Reflector.cs
@@ -7,7 +7,7 @@ public class Reflector : MonoBehaviour
     public void Awake() => player = LayerMask.GetMask("Player");
 
     private void OnTriggerEnter(Collider other) {
-        if (other.layer == player)
-            other.GetComponent<Rigidbody>().AddForce(forceDir, ForceMode.Impulse);
+        if (other.gameObject.layer == player)
+            other.transform.Rotate(Vector3.up * 180, Space.Self);
     }
 }
